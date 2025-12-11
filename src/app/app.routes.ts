a048baaea1db41fd2644bf00893a1f2239dadf0b
Stephen Fluin <stephen.fluin@gmail.com>
2025-12-11 09:45:12 -0600

    chore: restart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./features/scanner/scanner.component').then((m) => m.ScannerComponent),
-  },
-];
+export const routes: Routes = [];
